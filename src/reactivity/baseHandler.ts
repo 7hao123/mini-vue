83664dc2d835c05c77df3e673cad6d861b9bab27
qinhao <82450473@qq.com>
2025-12-17 10:20:14 +0800

    feat: readonly实现和get,set抽离baseHandler

---
+++ src/reactivity/baseHandler.ts
@@ -0,0 +1,41 @@
+import { track, trigger } from "./effect";
+// 只需要创建一个createGetter
+const get = createGetter();
+const set = createSetter();
+const readonlyGet = createGetter(true);
+// reactive和readonly的实现很相似，进行抽象
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+    const res = Reflect.get(target, key);
+    if (!isReadonly) {
+      track(target, key);
+    }
+    return res;
+  };
+}
+
+function createSetter() {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value);
+    // TODO 触发依赖
+    trigger(target, key);
+
+    return res;
+  };
+}
+
+export const mutableHandlers = {
+  get,
+  set,
+};
+
+export const readonlyHandlers = {
+  get: readonlyGet,
+  set(target, key, value) {
+    console.warn(
+      `key:"${String(key)}" set 失败，因为 target 是 readonly 类型`,
+      target
+    );
+    return true;
+  },
+};
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,20 +1,12 @@
-import { track, trigger } from "./effect";
+import { mutableHandlers, readonlyHandlers } from "./baseHandler";
 export function reactive(raw) {
-  return new Proxy(raw, {
-    // target是当前对象{foo:1}，key是用户访问的foo
-    get(target, key) {
-      const res = Reflect.get(target, key);
-      // TODO 依赖收集
-      track(target, key);
-      return res;
-    },
+  return createActiveObject(raw, mutableHandlers);
+}
 
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value);
+export function readonly(raw) {
+  return createActiveObject(raw, readonlyHandlers);
+}
 
-      // TODO 触发依赖
-      trigger(target, key);
-      return res;
-    },
-  });
+function createActiveObject(raw, baseHandlers) {
+  return new Proxy(raw, baseHandlers);
 }
