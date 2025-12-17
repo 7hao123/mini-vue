b5e112a4e9a85472ae8f87376d2f6552075dc137
qinhao <82450473@qq.com>
2025-12-17 13:58:18 +0800

    feat: 实现isReadOnly和isReactive

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -3,8 +3,23 @@ export function reactive(raw) {
   return createActiveObject(raw, mutableHandlers);
 }
 
+export const enum ReactiveFlags {
+  IS_REACTIVE = "is_reactive",
+  IS_READONLY = "is_readonly",
+}
+
 export function readonly(raw) {
   return createActiveObject(raw, readonlyHandlers);
+}
+
+export function isReactive(value) {
+  // 这里会触发get
+  // 需要转换成boolean类型,因为如果不是响应式对象会undefined
+  return !!value[ReactiveFlags.IS_REACTIVE];
+}
+
+export function isReadonly(value) {
+  return !!value[ReactiveFlags.IS_READONLY];
 }
 
 function createActiveObject(raw, baseHandlers) {
