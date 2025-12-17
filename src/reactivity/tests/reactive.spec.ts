2bce30bcdbf0b14d9862081c9a7f866681441084
qinhao <82450473@qq.com>
2025-12-17 15:39:25 +0800

    feat:实现isProxy

--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -1,13 +1,14 @@
-import { isReactive, reactive } from "../reactive";
+import { isProxy, isReactive, reactive } from "../reactive";
 describe("reactive", () => {
-  // it("happy path", () => {
-  //   const original = { foo: 1 };
-  //   const observed = reactive(original);
-  //   expect(observed).not.toBe(original);
-  //   expect(observed.foo).toBe(1);
-  //   expect(isReactive(observed)).toBe(true);
-  //   expect(isReactive(original)).toBe(false);
-  // });
+  it("happy path", () => {
+    const original = { foo: 1 };
+    const observed = reactive(original);
+    expect(observed).not.toBe(original);
+    expect(observed.foo).toBe(1);
+    expect(isReactive(observed)).toBe(true);
+    expect(isReactive(original)).toBe(false);
+    expect(isProxy(observed)).toBe(true);
+  });
   test("nested reactive", () => {
     const original = {
       nested: {
